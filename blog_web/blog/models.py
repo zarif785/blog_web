a26251bd804ad794d1e645d8b8e2b01cbb791a28
zarif_arefeen <zarifarefeen@gmail.com>
2022-10-16 23:14:03 +0600

    Post Model Added

--- blog_web/blog/models.py
+++ blog_web/blog/models.py
@@ -1,3 +1,14 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete = models.CASCADE) #this means if a user is deleted, delete the post too
+     
+    def __str__(self):
+        return f'{self.title} -- {self.author.username}'   
--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+
+def home(request):
+
+    return render(request,'blog/home.html',{
+        'posts':Post.objects.all(),
+        'title':"Home"
+    })
+
+
+def about(request):
+    return render(request,'blog/about.html',{
+        'title' :"About"
+    })
